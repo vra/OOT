7d75af58f90b331aac20f0d7a891db8d651fc0e6
vra <vra000@163.com>
2013-10-27 19:19:51 +0800

    add Ex5

---
+++ EX5/TestFLTK.c
@@ -0,0 +1,11 @@
+﻿#include <Fl/Fl.H>
+#include <FL/Fl_Box.H>
+#include<Fl/Fl_Window.H>
+
+int main()
+{
+	Fl_Window window(200,200,"Window title");
+	Fl_Box box(0,0,200,200,"Hey,I mean,Hello,World!");
+	window.show();
+	return Fl::run();
+}
